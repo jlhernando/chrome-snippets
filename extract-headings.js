005c329c9f31ac8f938f5685c7a9b2a44f759ae9
Jose Hernando <jose@builtvisible.com>
2021-03-03 00:49:32 +0000

    Updated ectract-headings: return object and console.table

--- extract-headings.js
+++ extract-headings.js
@@ -1,11 +1,12 @@
-// Author: Jose Luis Hernando | @jlhernando
 function getHeadings() {
   var headingsArr = Array.from(
     document.querySelectorAll('h1, h2, h3, h4, h5, h6')
   )
-  var headings = headingsArr.map((h) =>
-    console.log(`"${h.innerText}" as an ${h.tagName}`)
-  )
+  var headings = headingsArr.map((h) => {
+    const obj = { heading: h.innerText, tag: h.tagName }
+    return obj
+  })
+  console.table(headings)
   return `There are ${headings.length} headings`
 }
 getHeadings()
